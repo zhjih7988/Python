23a0f5d5edd9c6d1e6d73e7214f85075121e20d1
TopShares <1255877369@qq.com>
2018-01-25 09:05:13 +0800

    me

--- test.py
+++ test.py
@@ -19,18 +19,10 @@ import re
 
 def main():
     str = r"<html><body><h1>hello world<h1></body></html>"#这段是你要匹配的文本
-    p1 = r"(?<=<h1>).+?(?=<h1>)"#这是我们写的正则表达式规则，你现在可以不理解啥意思
-    pattern1 = re.compile(p1)#我们在编译这段正则表达式
-    
-    print(pattern1.findall(str))
+    p1 = r"(?<=<h1>).+?(?=<h1>)" #正则表达式规则
+    pattern1 = re.compile(p1) #编译这段正则表达式
     matcher1 = re.search(pattern1,str)#在源文本中搜索符合正则表达式的部分
-    print(matcher1.group(0))#打印出来
-
-    key = r"afiouwehrfuichuxiuhong@hit.edu.cnaskdjhfiosueh"
-    p1 = r"chuxiuhong@hit\.edu\.cn"
-    pattern1 = re.compile(p1)
-    print(pattern1.findall(key))
-
+    print(matcher1.group())#打印出来
 
 if __name__ == '__main__':
     main()
