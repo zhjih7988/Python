ff0a006c65230d3fbf35e769f731dcd4245da4f4
BangLong Liu <liub1922@gmail.com>
2018-02-04 11:09:15 +0800

    s

--- Spider/STorrent.py
+++ Spider/STorrent.py
@@ -1,1 +1,11 @@
 # coding: utf-8
+from lxml import html
+from MyFun import SaveToHtml
+
+parameter = '123'
+Url = 'http://torrentkitty.kim/tk/'+ parameter +'/1-1-0.html'
+page = html.parse(Url)
+root = page.getroot()
+
+list_divs = root.cssselect('list')
+# print(list_divs)
