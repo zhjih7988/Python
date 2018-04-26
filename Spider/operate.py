ec979ffb3b87e3215cf6b2684bdd42c03dd3fe2a
TopShares <1255877369@qq.com>
2018-04-26 15:03:45 +0800

    me

--- Spider/operate.py
+++ Spider/operate.py
@@ -8,28 +8,36 @@ def openPath(path=""):
     for filename in fileList:
         pathTmp = os.path.join(path,filename)   #获取path与filename组合后的路径
         if os.path.isdir(pathTmp):   #如果是目录
-            openPath(pathTmp) 
+            openPath(pathTmp)        #则递归查找
+            
+            out = getNumber(pathTmp)
+            if out is not None:
+                newPath = os.path.join(path,out) 
+                print(newPath)
+                os.renames(pathTmp,newPath)
             # out = getTen(pathTmp[-1:])
         #     if out is not 10:
         #         newPath = os.path.join(path,str(out))
         #         os.renames(pathTmp,newPath)
         #     print(pathTmp)
-        #     openPath(pathTmp)        #则递归查找
-        '''
+        
         else:
             print(filename)
             out = getNumber(filename)
             if out is not None:
                 newPath = os.path.join(path,out) 
                 os.renames(pathTmp,newPath)
-        '''
+        
 
 def main():
     # path=r"C:\Users\Pancras\Desktop\课件"
     # path = r'G:\安全\渗透教程\最新Kali渗透'
     # path = r'G:\fundamental  algorithms\12、程序猿的内功修炼，学好算法与数据结构'
     # path = r'G:\CS\19、高性能可扩展MySQL数据库设计及架构优化 电商项目\高性能可扩展mysql'
-    path = r'G:\CS\63、前端跳槽面试必备技巧'
+    # path = r'G:\CS\63、前端跳槽面试必备技巧'
+    # path = r'F:\数学\2019【启航数学】张宇\02 教材精讲班\线代'
+    # path = r'F:\数学\2019【启航数学】张宇\02 教材精讲班\概率'
+    path = r'C:\Users\Pancras\Desktop\a'
     openPath(path)
 
 
--- Spider/test.py
+++ Spider/test.py
@@ -1,4 +1,54 @@
+import os
+import os.path
 import re
-a = '【下资源xiazy.com】第24课 - 队列的特别实现'
-a= '第5章 三面四面(关注公众号菜鸟要飞，免费领取200G+教程)'
-a = re.sub(r'\(.*','',a)
+from MyFunFile import getNumber, getTen
+
+def openPath(path=""):
+    fileList = os.listdir(path)   #获取path目录下所有文件
+    for filename in fileList:
+        pathTmp = os.path.join(path,filename)   #获取path与filename组合后的路径
+        if os.path.isdir(pathTmp):   #如果是目录
+            openPath(pathTmp)        #则递归查找
+            # out = getNumber(pathTmp)
+            # if out is not None:
+            #     newPath = os.path.join(path,out) 
+            #     print(newPath)
+            #     os.renames(pathTmp,newPath)
+
+            # out = getTen(pathTmp[-1:])
+        #     if out is not 10:
+        #         newPath = os.path.join(path,str(out))
+        #         os.renames(pathTmp,newPath)
+        #     print(pathTmp)
+        
+        else:
+            # a = pathTmp.split('\\')[:-2]
+            # newPath = ''
+            # for i in a:
+            #     newPath = newPath + i + '\\'
+            # print(pathTmp)
+            # newPath = os.path.join(newPath,filename)
+            # print(newPath)
+            # os.renames(pathTmp,newPath)
+            '''
+
+            '''
+            out = getNumber(filename)
+            if out is not None:
+                newPath = os.path.join(path,out) 
+                os.renames(pathTmp,newPath)
+
+def main():
+    path = r'C:\Users\Pancras\Desktop\概率'
+    path = r'F:\数学\2019【启航数学】张宇\02 教材精讲班\概率'
+    path = r'F:\数学\2019【启航数学】张宇\02 教材精讲班\高数\高数下'
+    path = r'F:\数学\2019【启航数学】张宇\02 教材精讲班\线代'
+    openPath(path)
+    # count = 0
+    # for root, dirs, files in os.walk(path):
+    #     for each in files:
+    #         count +=1
+    # print(count)
+    
+if __name__ == '__main__':
+    main()
